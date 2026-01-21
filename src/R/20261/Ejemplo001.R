b31a5dc5c2a4de62e9711142b0c1c9496bba39d2
ibarram <ibarram@mac.com>
2026-01-20 21:06:51 -0600

    Update Ejemplo001.R

--- src/R/20261/Ejemplo001.R
+++ src/R/20261/Ejemplo001.R
@@ -23,11 +23,18 @@ vf <- Vectorize(f, vectorize.args = "sl")
 # Admitidos por anio
 naat <- vf(datos$anio, lb_a, datos$admitidos_total)
 naah <- vf(datos$anio, lb_a, datos$admitidos_hombres)
-naam <- vf(datos$anio, lb_a, datos$admitidas_mujeress)
+naam <- vf(datos$anio, lb_a, datos$admitidas_mujeres)
 t_ah <- naah/naat
 t_am <- naam/naat
 
+# Grafica de aceptació por género desde 2000 a 2024
+par(mar = c(6, 4, 4, 2) + 0.1, xpd = NA)
 plot(lb_a, t_ah, type = 'o', xlab = "Año", ylab="Tasa", lwd = 2,
-     col = "blue", ylim = c(.6, .8), lty = "solid", pch = 19,
-     main = "Tasa de aceptación de hombre")
+     col = "blue", ylim = c(.2, .8), lty = "solid", pch = 19,
+     main = "Tasa de aceptación por género")
+grid()
+lines(lb_a, t_am, type = 'o', lwd = 2,
+     col = "red", lty = "solid", pch = 19)
+legend("bottomleft", inset = c(0, -1), legend = c("Hombres", "Mujeres"),
+       col = c("blue", "red"), lty = 1, pch = 19, lwd = 2, bty = "n")
 
