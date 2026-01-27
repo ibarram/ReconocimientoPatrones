ded2c8632fcb3de0fc12e712691c46eca2d22640
ibarram <ibarram@mac.com>
2026-01-27 13:09:44 -0600

    Update

--- src/R/20261/Ejemplo002.R
+++ src/R/20261/Ejemplo002.R
@@ -12,14 +12,18 @@ sz <- dim(datos)
 
 # Selección de los único de las columnas de anio, facultad y escuela
 lb_a <- unique(datos$anio)
+nlb_a <- length(lb_a)
+vlb_a <- 1:nlb_a
 lb_f <- unique(datos$facultad)
+nlb_f <- length(lb_f)
+vlb_f <- 1:nlb_f
 lb_c <- unique(datos$carrera)
+nlb_c <- length(lb_c)
+vlb_c <- 1:nlb_c
 
 # Función para la suma en base de dos caractersiticas seleccionadas
 f <- function(vct1, sl1, vct2, sl2, vtc_d) smn <- sum(vtc_d[vct1==sl1&vct2==sl2])
 # Vectorización de la selección
-vf <- Vectorize(f, vectorize.args = c("sl1","sl2"))
-
 vf1 <- Vectorize(f, vectorize.args = "sl1")
 vf2 <- Vectorize(vf1, vectorize.args = "sl2")
 
@@ -32,14 +36,11 @@ t_am <- naam/naat
 ft <- t_am/t_ah
 
 # Grafica de aceptación por género desde 2000 a 2024
-par(mar = c(6, 4, 4, 2) + 0.1, xpd = NA)
-plot(lb_a, ft[,lb_f[1]], type = 'o', xlab = "Año", ylab="Tasa", lwd = 2,
-     col = "blue", ylim = c(.2, 6), lty = "solid", pch = 19,
-     main = "Tasa de aceptación por género")
+matplot(ft, type = "o", xlab = "Año", ylab="Tasa", lwd = 4, 
+        col = vlb_f, ylim = c(min(ft), max(ft)), lty = 1, pch = vlb_f, 
+        main="Tasa de aceptación por género")
+palette("R3"); 
 grid()
+legend("topleft", lb_f, col=vlb_f, pch = vlb_f, 
+       text.col = "gray", title= "Facultades")
 
-for(i_f in lb_f)
-{
-lines(lb_a, ft[,i_f], type = 'o', lwd = 2,
-      col = "red", lty = "solid", pch = 19)
-}
