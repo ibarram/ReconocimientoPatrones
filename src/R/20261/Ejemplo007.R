f81e919cba37f81ff73ff230bb72a9b453bbf405
ibarram <ibarram@mac.com>
2026-01-28 14:15:26 -0600

    Update Ejemplo007.R

--- src/R/20261/Ejemplo007.R
+++ src/R/20261/Ejemplo007.R
@@ -6,6 +6,7 @@ data <- read.table(filename, sep = "", header = FALSE)
 
 x <- as.numeric(data[1,])
 ix <- order(x)
+n <- length(x)
 y <- as.numeric(data[2,])
 p <- 15
 
@@ -45,8 +46,23 @@ e_R2 <- function(y, yl)
   return(1 - SSE / s2y)
 }
 
+r_mSo <- function(x, y, p, xi)
+{
+  stopifnot(xi %in% x)
+  ix <- x != xi
+  
+  x <- x[ix]
+  y <- y[ix]
+  
+  a <- r_mS(x, y, p)
+  yl <- s_mS(a, x)
+  R2 <- e_R2(y, yl)
+  return(R2)
+}
+
 vr_mS <- Vectorize(r_mS, vectorize.args = "p")
 vs_mS <- Vectorize(s_mS, vectorize.args = "a")
+vr_mSo <- Vectorize(r_mSo, vectorize.args = "xi")
 
 vp <- 1:p
 a <- vr_mS(x, y, vp)
@@ -59,6 +75,11 @@ p_sel <- min(vp[R2>R2_th])
 a_sel <- a[[p_sel]]
 y_sel <- yl[,p_sel]
 
+R2o <- vr_mSo(x, y, p_sel, x)
+R2o_th <- mean(R2o) + qt(0.995, df = n-1) * sd(R2o)/sqrt(n)
+id_o <- which(R2o>R2o_th)
+
 plot(x, y, col = "red", pch = 19)
 lines(x[ix], y_sel[ix], col = "blue", type = "l", lwd = 4)
+points(x[id_o], y[id_o], col = "blue", pch = 1, cex = 2, lwd = 4)
 grid()
